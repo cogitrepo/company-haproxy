3dbe4a6e9c7137e9ecfb29c38ff16dc68a357f2b
David Camara <jdpcii@gmail.com>
2019-08-15 16:07:43 -0400

    FIxing Layout/AlignArray rubocop

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,9 @@
-default['haproxy']['members'] = [{
-  'hostname' => 'web1',
-  'ipaddress' => '10.1.1.3',
-  'port' => 80,
-  'ssl_port' => 443,
-},
-{
-  'hostname' => 'web2',
-  'ipaddress' => '10.1.1.4',
-  'port' => 80,
-  'ssl_port' => 443,
-}]
+default['haproxy']['members'] = [{'hostname'  => 'web1',
+                                  'ipaddress' => '10.1.1.3',
+                                  'port'      => 80,
+                                  'ssl_port'  => 443, },
+                                 {
+                                  'hostname'  => 'web2',
+                                  'ipaddress' => '10.1.1.4',
+                                  'port'      => 80,
+                                  'ssl_port'  => 443,}]
